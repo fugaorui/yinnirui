595b2968513fb2725af916c73e9259eeae5f538d
fugaorui <32642822@qq.com>
2018-10-16 10:27:20 +0800

    测试

---
+++ 测试文件夹/2.sql
@@ -0,0 +1,11 @@
+﻿select s.id from mtsh_product_spu s where s.id =196324;
+
+select t.* from om_orders_details t where t.product_spu_id not in(
+select s.id from mtsh_product_spu s);
+
+select t.orders_id from om_orders_details t where t.product_spu_id not in(
+select s.id from mtsh_product_spu s);
+
+
+select t.* from om_orders_details t;
+
